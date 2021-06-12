55354d4717009dcd21da74a177519e12bfbf972c
zhhlbaw2011@gmail.com <zhhlbaw2011@gmail.com>
2021-06-12 09:45:39 -0400

    Add project files.

---
+++ GmailBot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GmailBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
